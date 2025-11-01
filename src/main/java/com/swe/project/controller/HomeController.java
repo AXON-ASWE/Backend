b8e5f04b0b509fd250f3073dea9503c672f60a41
RavenTheshadow <hogiathang2@gmail.com>
2025-11-01 16:43:04 +0700

    Add Cors

---
+++ src/main/java/com/swe/project/controller/HomeController.java
@@ -0,0 +1,31 @@
+package com.swe.project.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.view.RedirectView;
+
+import io.swagger.v3.oas.annotations.Hidden;
+
+@Controller
+public class HomeController {
+
+    /**
+     * Redirect root path to Swagger UI
+     * This endpoint is hidden from Swagger documentation
+     */
+    @Hidden
+    @GetMapping("/")
+    public RedirectView redirectToSwagger() {
+        return new RedirectView("/swagger-ui.html");
+    }
+
+    /**
+     * Alternative home endpoint that also redirects to Swagger UI
+     * This endpoint is hidden from Swagger documentation
+     */
+    @Hidden
+    @GetMapping("/home")
+    public RedirectView homeToSwagger() {
+        return new RedirectView("/swagger-ui.html");
+    }
+}
